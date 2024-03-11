aaeb084656d17144b713fecb2510e858574e9627
Eliana Setiabudi <56292047+elseti@users.noreply.github.com>
2024-03-11 01:00:53 -0500

    fix: modified user input style

--- src/main.cpp
+++ src/main.cpp
@@ -20,50 +20,58 @@ std::set<std::string> valid_projects = {"COAP", "DJANGO", "BLE"};
 int main(int argc, char* argv[]) {
     string project_type, url, request_type, response, line, input_file_path;
 
-    // Check for no. of arguments
-    if (argc < 5) {
-        std::cerr << "Error: Invalid arguments provided." << endl;
-        std::cerr << "The expected format is:" << endl;
-        std::cerr << std::string(argv[0]) << " <project type> <url> <request_type> <input_file_path>" << endl;
-        return 1;
+    // Input project_type
+    while(true){
+        std::cout << "What fuzzing target are you testing? [Options: COAP, DJANGO, BLE]" << endl;
+        std::cin >> project_type;
+        std::transform(project_type.begin(), project_type.end(), project_type.begin(), ::toupper);
+        if (!valid_projects.count(project_type)) {
+            cerr << "Invalid project type. Please enter COAP/DJANGO/BLE.\n" << endl;
+        } else {
+            break; 
+        }
     }
-
-    project_type = argv[1];
-    url = argv[2];
-    request_type = argv[3];
-
-    // Project_type & Request_type convert to upper case, url im not checking
-    std::transform(request_type.begin(), request_type.end(), request_type.begin(), ::toupper);
-    std::transform(project_type.begin(), project_type.end(), project_type.begin(), ::toupper);
-
-    // If proejct_type & request_type is correct
-    if (!valid_projects.count(project_type)) {
-        cerr << "Invalid project type. Please enter COAP/DJANGO/BLE." << endl;
-        return 1;
+    
+    // Input URL
+    std::cout << "\nWhat URL are you testing on?" << endl;
+    std::cin >> url;
+    
+    // Input request_type
+    while(true){
+        std::cout << "\nWhat is your request type? [Options: GET, POST, PUT, HEAD, DELETE]" << endl;
+        std::cin >> request_type;
+        std::transform(request_type.begin(), request_type.end(), request_type.begin(), ::toupper);
+        if (!valid_request_types.count(request_type)) {
+            cerr << "Invalid request type. Please enter GET/POST/PUT/HEAD/DELETE.\n" << endl;
+        } else {
+            break;
+        }
     }
-    if (!valid_request_types.count(request_type)) {
-        cerr << "Invalid request type. Please enter GET/POST/PUT/HEAD/DELETE." << endl;
-        return 1;
-    }
-
-    input_file_path = argv[4];
+    
+    // Input input_file_path
     std::ifstream input_file(input_file_path);
-    if (!input_file.is_open()) {
-        std::cerr << "Error: Could not open file '" << input_file_path << "'." << endl;
-        return 1;
+    while(true){
+        std::cout << "\nWhat is your seed input's file path?" << endl;
+        std::cin >> input_file_path;
+        std::ifstream input_file(input_file_path);
+        if (!input_file.is_open()) {
+            std::cerr << "Error: Could not open file '" << input_file_path << "'." << endl;
+        } else {
+            break;
+        }
     }
-
+    
     // This is where you guys call your functions 
     if (project_type == "COAP") {
-        std::cout << "[COAP] Fuzzer has initiated call to COAP!" << endl;
+        std::cout << "\n[COAP] Fuzzer has initiated call to COAP!" << endl;
         CoAP_Handler();
 
     } else if (project_type == "DJANGO") {
-        std::cout << "[DJANGO] Fuzzer has initiated call to DJANGO Web Application!" << endl;
+        std::cout << "\n[DJANGO] Fuzzer has initiated call to DJANGO Web Application!" << endl;
         Django_Handler(url, request_type, input_file_path);
 
     } else if (project_type == "BLE") {
-        std::cout << "[BLE] Fuzzer has initiated call to BLE_Zephyr!" << endl;
+        std::cout << "\n[BLE] Fuzzer has initiated call to BLE_Zephyr!" << endl;
         BLE_Zephyr_Handler();
 
     } else {
