0b03ce5f03ba01aef8c4cc5f367272c03e97dcd8
Goh Nicholas <nicholasgoh62@gmail.com>
2024-03-11 14:23:25 +0800

    fix: Updated

--- src/Django_Web/django_web.h
+++ src/Django_Web/django_web.h
@@ -1,6 +1,6 @@
 #ifndef DJANGO_WEB_H
 #define DJANGO_WEB_H
 
-void Django_Handler(string url, string request_type);
+void Django_Handler(string url, string request_type, string input_file_path);
 
 #endif
