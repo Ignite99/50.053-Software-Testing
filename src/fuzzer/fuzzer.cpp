f4581887332ae2bbb839692c4776174d82931a75
Goh Nicholas <nicholasgoh62@gmail.com>
2024-03-12 17:16:17 +0800

    feat: bruteforced fuzzer for integer and string

--- src/fuzzer/fuzzer.cpp
+++ src/fuzzer/fuzzer.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
+#include <random>
+#include <nlohmann/json.hpp>
 #include "fuzzer.h"
-
-using namespace std;
 
 string bit_flip(const string& input) {
   random_device rd;
@@ -38,3 +38,51 @@ string delete_byte(const string& input) {
   int pos = dist(gen);
   return input.substr(0, pos) + input.substr(pos + 1);
 }
+
+string randomize_string(int length) {
+    static const char alphanum[] =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distr(0, sizeof(alphanum) - 2);
+
+    std::string random_string;
+    random_string.reserve(length);
+
+    for (int i = 0; i < length; ++i) {
+        random_string += alphanum[distr(gen)];
+    }
+
+    return random_string;
+}
+
+/* ============================== DJANGO FUZZER ============================== */
+
+// Django JSON fuzzer
+void randomizerJSON(nlohmann::json& j) {
+  int num_value;
+  string string_value;
+
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> int_dist(0, 100);
+  std::uniform_real_distribution<> float_dist(0.0, 1.0);
+  std::uniform_int_distribution<> bool_dist(0, 1);
+
+  for (auto& [key, value] : j.items()) {
+    if (value.is_number()) {
+      num_value = int_dist(gen);
+      j[key] = num_value;
+    } else if (value.is_string()) {
+      string_value = randomize_string(50);
+      j[key] = string_value;
+    }
+    std::cout << "Value of field " << key << ": " << value << endl;
+  }
+}
+
+
+/* ============================== DJANGO FUZZER ============================== */
