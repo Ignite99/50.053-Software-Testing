62bf9264fb50655ed86f26cbc5529284a200fd67
Goh Nicholas <nicholasgoh62@gmail.com>
2024-04-01 21:38:16 +0800

    chore: replaced main.cpp -> main.go

---
+++ golang_src/main.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+var (
+	validProjectTypes = map[string]bool{
+		"COAP":   true,
+		"DJANGO": true,
+		"BLE":    true,
+	}
+	validRequestTypes = map[string]bool{
+		"GET":    true,
+		"POST":   true,
+		"PUT":    true,
+		"HEAD":   true,
+		"DELETE": true,
+	}
+)
+
+func main() {
+	var projectType string
+	var url string
+	var requestType string
+
+	if len(os.Args) != 6 && len(os.Args) != 1 {
+		fmt.Println("Error: Invalid arguments provided.")
+		fmt.Println("The expected format is:")
+		fmt.Println(os.Args[0], "<project type> <url> <request_type> <input_file_path> <output_file_path>")
+		return
+	}
+
+	if len(os.Args) == 6 {
+		projectType = os.Args[1]
+		url = os.Args[2]
+		requestType = strings.ToUpper(os.Args[3])
+
+		if !validProjectTypes[projectType] {
+			fmt.Println("Invalid project type. Please enter COAP/DJANGO/BLE.")
+			return
+		}
+
+		if !validRequestTypes[requestType] {
+			fmt.Println("Invalid request type. Please enter GET/POST/PUT/HEAD/DELETE.")
+			return
+		}
+
+		inputFile, err := os.Open(os.Args[4])
+		if err != nil {
+			fmt.Println("Error: Could not open file", os.Args[4], err)
+			return
+		}
+		defer inputFile.Close()
+
+		outputFile, err := os.Open(os.Args[5])
+		if err != nil {
+			fmt.Println("Error: Could not open file", os.Args[5], err)
+			return
+		}
+		defer outputFile.Close()
+	} else {
+		fmt.Println("What fuzzing target are you testing? [Options: COAP, DJANGO, BLE]")
+		var err error
+		for {
+			_, err = fmt.Scanln(&projectType)
+			if err != nil {
+				fmt.Println("Error reading input:", err)
+				continue
+			}
+			projectType = strings.ToUpper(projectType)
+			if validProjectTypes[projectType] {
+				break
+			}
+			fmt.Println("Invalid project type. Please enter COAP/DJANGO/BLE.")
+		}
+
+		fmt.Println("\nWhat URL are you testing on?")
+		_, err = fmt.Scanln(&url)
+		if err != nil {
+			fmt.Println("Error reading input:", err)
+			return
+		}
+
+		fmt.Println("\nWhat is your request type? [Options: GET, POST, PUT, HEAD, DELETE]")
+		for {
+			_, err = fmt.Scanln(&requestType)
+			if err != nil {
+				fmt.Println("Error reading input:", err)
+				continue
+			}
+			requestType = strings.ToUpper(requestType)
+			if validRequestTypes[requestType] {
+				break
+			}
+			fmt.Println("Invalid request type. Please enter GET/POST/PUT/HEAD/DELETE.")
+		}
+
+		var inputFilePath string
+		for {
+			fmt.Println("\nWhat is your seed input's file path?")
+			_, err = fmt.Scanln(&inputFilePath)
+			if err != nil {
+				fmt.Println("Error reading input:", err)
+				continue
+			}
+			inputFile, err := os.Open(inputFilePath)
+			if err == nil {
+				defer inputFile.Close()
+				break
+			}
+		}
+
+		var outputFilePath string
+		for {
+			fmt.Println("\nWhat is your output file path?")
+			_, err = fmt.Scanln(&outputFilePath)
+			if err != nil {
+				fmt.Println("Error reading input:", err)
+				continue
+			}
+			outputFile, err := os.Open(outputFilePath)
+			if err == nil {
+				defer outputFile.Close()
+				break
+			}
+		}
+	}
+
+	if projectType == "COAP" {
+		fmt.Println("[COAP] Fuzzer has initiated call to COAP!")
+		// CoAP_Handler()
+	} else if projectType == "DJANGO" {
+		fmt.Println("[DJANGO] Fuzzer has initiated call to DJANGO Web Application!")
+		// Django_Test_Driver(1, url, request_type, input_file_path, output_file_path)
+
+	} else if projectType == "BLE" {
+		fmt.Println("[BLE] Fuzzer has initiated call to BLE_Zephyr!")
+		// BLE_Zephyr_Handler()
+
+	} else {
+		fmt.Println("Project type mutated. Project type: " + projectType + ". Check code now!")
+		return
+	}
+
+}
